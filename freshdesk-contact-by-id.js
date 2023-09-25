a0907857d3cf844108dea1bcfe5b2e0022e9d0a9
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2023-09-25 14:58:34 +0200

    Fixed authentication

--- freshdesk-contact-by-id.js
+++ freshdesk-contact-by-id.js
@@ -12,17 +12,20 @@ module.exports = function (RED) {
 
         // Define the function to call the Freshdesk API directly
         this.getContactById = function () {
+
             // Set up the Axios request with Basic Authentication header
-            //const authHeader = `Basic ${Buffer.from(node.apiKey + ':X').toString('base64')}`;
+            const apiKey = node.apiKey.trim(); // Trim leading/trailing white spaces
+            const authHeaderNew = `Basic ${Buffer.from(apiKey + ':X').toString('base64')}`;
             const authHeader = "Basic UHBFOGNrMThValRGdzBRME50Og==";
             const axiosConfig = {
                 headers: {
-                    'Authorization': authHeader,
+                    'Authorization': authHeaderNew,
                     //'Content-Type': 'application/json',
                 },
             };
 
             // Make a GET request to the Freshdesk API
+            node.warn(node.apiKey);
             node.warn(axiosConfig);
             //console.log('This is a server-side debugging message');
             axios.get(`https://jonesybinc.freshdesk.com/api/v2/contacts/${node.contactId}`, axiosConfig)
@@ -48,3 +51,4 @@ module.exports = function (RED) {
 
     RED.nodes.registerType('freshdesk-contact-by-id', FreshdeskContactByIdNode);
 };
+
