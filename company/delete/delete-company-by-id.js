a9e1d2a1fbf526e18f7b74cc23f517f18af54e46
Jonas de Bruin <jonasdebruin05@gmail.com>
2023-09-28 13:34:27 +0200

    Added delete company node
    (cherry picked from commit 8e108de47bbb29b50702c4be0e55ed437b12c7e1)

--- company/delete/delete-company-by-id.js
+++ company/delete/delete-company-by-id.js
@@ -1,19 +1,19 @@
 const axios = require('axios');
 
 module.exports = function (RED) {
-    function FreshdeskDeleteContactByIdNode(config) {
+    function FreshdeskDeleteCompanyByIdNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         this.credentials = RED.nodes.getNode(config.freshdesk);
 
         // Retrieve configuration values from node
-        this.contactId = config.contactId.trim();
+        this.companyId = config.companyId.trim();
         this.domain = this.credentials.domain;
         this.apiKey = this.credentials.apiKey;
 
         // Define the function to call the Freshdesk API directly
         this.deleteContactById = function (msg) {
-            this.contactId = msg.id;
+            this.companyId = msg.id;
             // Set up the Axios request with Basic Authentication header
             const authHeader = `Basic ${Buffer.from(this.apiKey + ':X').toString('base64')}`;
             const axiosConfig = {
@@ -24,9 +24,9 @@ module.exports = function (RED) {
             };
 
             // Make a GET request to the Freshdesk API
-            axios.delete(`https://${this.domain}.freshdesk.com/api/v2/contacts/${this.contactId}`, axiosConfig)
+            axios.delete(`https://${this.domain}.freshdesk.com/api/v2/companies/${this.companyId}`, axiosConfig)
                 .then((response) => {
-                    // Send the contactData to the next node
+                    // Send the companyData to the next node
                     node.send({ payload: "Deleted contact"});
                 })
                 .catch((error) => {
@@ -41,5 +41,5 @@ module.exports = function (RED) {
             node.deleteContactById(msg);
         });
     }
-    RED.nodes.registerType('freshdesk-delete-contact-by-id', FreshdeskDeleteContactByIdNode);
+    RED.nodes.registerType('freshdesk-delete-company-by-id', FreshdeskDeleteCompanyByIdNode);
 };
